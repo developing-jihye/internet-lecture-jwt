b15790d4571e9ea782c5987eed388e6a736771c1
Jihye Kim <wlgp115@gmail.com>
2024-07-13 17:37:18 +0900

    Revert "[API 구현] 강의 하나 등록(수강생 관련 제외)"
    This reverts commit b1ea722fc73e97f3edf9d095e96d2ce2c23518b1.

--- src/main/java/practice/internet_lecture/Course/CourseCategory.java
+++
@@ -1,9 +0,0 @@
-package practice.internet_lecture.Course;
-
-public enum CourseCategory {
-    국어,
-    수학,
-    영어,
-    과학,
-    사회
-}
--- src/main/java/practice/internet_lecture/instructor/InstructorRepository.java
+++
@@ -1,6 +0,0 @@
-package practice.internet_lecture.instructor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InstructorRepository extends JpaRepository<Instructor, Long> {
-}
