b1ea722fc73e97f3edf9d095e96d2ce2c23518b1
Jihye Kim <wlgp115@gmail.com>
2024-07-13 17:32:52 +0900

    [API 구현] 강의 하나 등록(수강생 관련 제외)

---
+++ src/main/java/practice/internet_lecture/instructor/InstructorRepository.java
@@ -0,0 +1,6 @@
+package practice.internet_lecture.instructor;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InstructorRepository extends JpaRepository<Instructor, Long> {
+}
