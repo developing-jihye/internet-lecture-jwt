5c0b532c5d82bfbd15bb3c3bbf0a4fc124911760
Jihye Kim <wlgp115@gmail.com>
2024-07-13 15:34:01 +0900

    Initial commit

---
+++ src/main/java/practice/internet_lecture/InternetLectureApplication.java
@@ -0,0 +1,13 @@
+package practice.internet_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/practice/internet_lecture/InternetLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.internet_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternetLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
